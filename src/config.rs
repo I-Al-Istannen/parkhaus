a9b39f83e2493cb36880b8e8ea2e33e9d663f335
two-heart <12869538+two-heart@users.noreply.github.com>
2026-03-20 12:45:11 +0100

    fix coldest_upstream

--- src/config.rs
+++ src/config.rs
@@ -119,7 +119,7 @@ impl Config {
     pub fn coldest_upstream(&self) -> &Upstream {
         self.upstreams
             .values()
-            .min_by_key(|u| u.order)
+            .max_by_key(|u| u.order)
             .expect("config must contain at least one upstream")
     }
 }
@@ -140,6 +140,7 @@ impl Debug for S3Secret {
 #[derive(Debug, Clone)]
 pub struct Upstream {
     pub name: UpstreamId,
+    /// The order of the upstream, where smaller numbers are hotter.
     pub order: usize,
     pub base_url: Url,
     pub addressing_style: AddressingStyle,
