e87f56f63937cdf36bd306dfb3ce068918aeef60
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2026-04-08 21:05:45 +0200

    Revert "Include certificate data in docker image"
    This reverts commit e30250226bbad395ef2fd5bc4ed4d7d9e51760fc.

--- flake.nix
+++ flake.nix
@@ -88,7 +88,7 @@
             config = {
               Entrypoint = [ "${pkgs.lib.getExe parkhaus}" ];
               # The config uses the local timezone
-              Env = [ "TZDIR=${pkgs.tzdata}/share/zoneinfo" "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt" ];
+              Env = [ "TZDIR=${pkgs.tzdata}/share/zoneinfo" ];
 
               ExposedPorts = {
                 "8080/tcp" = { };
