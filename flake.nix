9aa938b503fbea6073bebd2d118b90eb1d9e4320
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2026-04-08 18:37:15 +0200

    Switch to native-tls
    Maybe this squashes some weird http2 errors, namely
    
    - reqwest client: http2 error: stream error sent by user: unexpected
      internal error encountered
    - traefik: 499 (client cancelled)
    - garage: 500, hyper error, error reading a body from connection

--- flake.nix
+++ flake.nix
@@ -36,6 +36,11 @@
               version = (pkgs.lib.importTOML ./Cargo.toml).package.version;
               doCheck = false;
 
+              nativeBuildInputs = [
+                pkgs.pkg-config
+                pkgs.openssl
+              ];
+
               src = pkgs.lib.cleanSourceWith {
                 filter = gitignoreFilterWith {
                   basePath = ./.;
